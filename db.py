bf131070f5ae6c782f9855ea79ce0ac4a883effb
Daniil Shadrin <rabbit72rus@gmail.com>
2018-09-28 15:49:10 +0300

    separate models and db

--- db.py
+++ db.py
@@ -1,99 +1,47 @@
 import sqlalchemy
-from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.exc import OperationalError
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+import models
 
 # dialect+driver://username:password@host:port/database
 db = sqlalchemy.create_engine("postgresql://postgres:pass@localhost/")
-
-base = declarative_base()
-
-
-class Author(base):
-    __tablename__ = "author"
-    id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
-    name = Column(String, unique=True, nullable=False)
-
-    def __init__(self, name):
-        self.name = name
-
-    def __repr__(self):
-        return f"<Author({self.name})>"
-
-
-class Book(base):
-    __tablename__ = "book"
-    id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
-    title = Column(String, nullable=False)
-    year = Column(Integer, nullable=True)
-
-    def __init__(self, title, year=None):
-        self.title = title
-        self.year = year
-
-    def __repr__(self):
-        return f"<Book({self.title})>"
-
-
-class BooksAuthors(base):
-    __tablename__ = "books_authors"
-    author_id = Column(
-        Integer, ForeignKey("author.id"), nullable=False, primary_key=True
-    )
-    book_id = Column(Integer, ForeignKey("book.id"), nullable=False, primary_key=True)
-
-    def __init__(self, book_id, author_id):
-        self.book_id = book_id
-        self.author_id = author_id
-
-    def __repr__(self):
-        return f"<Book-Author({self.book_id} - {self.author_id})>"
-
 
 Session = sessionmaker(db)
 SESSION = Session()
 
 try:
-    base.metadata.create_all(db)
+    models.base.metadata.create_all(db)
 except OperationalError:
     exit("Check connection to DB")
 
 
 def get_statistic():
-    authors = SESSION.query(Author)
-    books = SESSION.query(Book)
+    authors = SESSION.query(models.Author)
+    books = SESSION.query(models.Book)
     return len(tuple(books)), len(tuple(authors))
 
 
-def add_book(title: str, year: int = None, authors: set = None):
+def add_book(title: str, year: int = None, authors: set = None, update=False):
     book = get_book(title, year, authors)
 
-    if book:  # and flag_update == True then update_book
+    if book:  # and update is True then update_book
         return
 
-        # book_authors = get_book_author_names(book.id)
-        # if book_authors == authors:
-        #     print("100% the same book")
-        #     return
-        # else:
-        #     print("Book exists but authors are different")
-        #     return
-
-    SESSION.add(Book(title, year))
+    SESSION.add(models.Book(title, year))
     SESSION.commit()
     _id = get_book(title, year, set()).id
     create_links_book_author(_id, get_authors_id(authors, add_new_authors=True))
 
 
 def add_author(name: str):
-    SESSION.add(Author(name))
+    SESSION.add(models.Author(name))
     SESSION.commit()
 
 
 def get_author(name: str):
     try:
-        author = SESSION.query(Author).filter_by(name=name)[0]
+        author = SESSION.query(models.Author).filter_by(name=name)[0]
     except IndexError:
         author = None
     return author
@@ -116,7 +64,7 @@ def get_book_author_names(_id: int) -> set:
 
 
 def get_book(title: str, year: int, authors: set):
-    books = list(SESSION.query(Book).filter_by(title=title, year=year))
+    books = list(SESSION.query(models.Book).filter_by(title=title, year=year))
     for book in books:
         if get_book_author_names(book.id) == authors:
             return book
@@ -127,8 +75,8 @@ def create_links_book_author(book_id, authors_id: dict):
     for author_id in authors_id.values():
         if not authors_id:
             continue
-        SESSION.add(BooksAuthors(book_id, author_id))
-        SESSION.commit()
+        SESSION.add(models.BooksAuthors(book_id, author_id))
+    SESSION.commit()
 
 
 def get_authors_id(author_names: set, add_new_authors=False) -> dict:
