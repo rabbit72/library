c9614445f3fc8ebebc88089cbb8097d3efe6304d
Daniil Shadrin <rabbit72rus@gmail.com>
2018-09-28 19:41:01 +0300

    add functions

--- db.py
+++ db.py
@@ -22,16 +22,14 @@ def get_statistic():
     return len(tuple(books)), len(tuple(authors))
 
 
-def add_book(title: str, year: int = None, authors: set = None, update=False):
+def add_book(title: str, year: int = None, authors: set = frozenset(), update=False):
     book = get_book(title, year, authors)
-
-    if book:  # and update is True then update_book
+    if book:
         return
-
     SESSION.add(models.Book(title, year))
     SESSION.commit()
     _id = get_book(title, year, set()).id
-    create_links_book_author(_id, get_authors_id(authors, add_new_authors=True))
+    __create_links_book_author(_id, __get_authors_id(authors, add_new_authors=True))
 
 
 def add_author(name: str):
@@ -47,7 +45,39 @@ def get_author(name: str):
     return author
 
 
-def get_book_author_names(_id: int) -> set:
+def get_book(title: str, year: int, authors: set):
+    books = list(SESSION.query(models.Book).filter_by(title=title, year=year))
+    for book in books:
+        if __get_book_author_names(book.id) == authors:
+            return book
+    return None
+
+
+def delete_book_by_id(_id: int) -> bool:
+    try:
+        book = SESSION.query(models.Book).filter_by(id=_id).delete()
+        SESSION.commit()
+    except:
+        SESSION.rollback()
+        book = False
+    return bool(book)
+
+
+def delete_all_data():
+    try:
+        num_deleted_books = SESSION.query(models.Book).delete()
+        num_deleted_authors = SESSION.query(models.Author).delete()
+        SESSION.commit()
+    except:
+        SESSION.rollback()
+        num_deleted_books, num_deleted_authors = 0, 0
+    return {
+        "book": num_deleted_books,
+        "author": num_deleted_authors,
+    }
+
+
+def __get_book_author_names(_id: int) -> set:
     request = SESSION.execute(
         """
         SELECT name FROM author
@@ -63,15 +93,7 @@ def get_book_author_names(_id: int) -> set:
     return set()
 
 
-def get_book(title: str, year: int, authors: set):
-    books = list(SESSION.query(models.Book).filter_by(title=title, year=year))
-    for book in books:
-        if get_book_author_names(book.id) == authors:
-            return book
-    return None
-
-
-def create_links_book_author(book_id, authors_id: dict):
+def __create_links_book_author(book_id, authors_id: dict):
     for author_id in authors_id.values():
         if not authors_id:
             continue
@@ -79,7 +101,7 @@ def create_links_book_author(book_id, authors_id: dict):
     SESSION.commit()
 
 
-def get_authors_id(author_names: set, add_new_authors=False) -> dict:
+def __get_authors_id(author_names: set, add_new_authors=False) -> dict:
     authors_id = dict()
     for name in author_names:
         author = get_author(name)
@@ -91,22 +113,3 @@ def get_authors_id(author_names: set, add_new_authors=False) -> dict:
                 author = None
         authors_id[name] = author.id
     return authors_id
-
-
-# # Create
-# doctor_strange = Film(title="Doctor Strange", director="Scott Derrickson", year="2016")
-# session.add(doctor_strange)
-# session.commit()
-#
-# # Read
-# films = session.query(Film)
-# for film in films:
-#     print(film.title)
-#
-# # Update
-# doctor_strange.title = "Some2016Film"
-# session.commit()
-#
-# # Delete
-# session.delete(doctor_strange)
-# session.commit()
