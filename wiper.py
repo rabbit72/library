ade85eff7ade876813d2e36768874ef0eb6aeea4
Daniil Shadrin <rabbit72rus@gmail.com>
2018-09-28 18:43:24 +0300

    init

--- wiper.py
+++ wiper.py
@@ -1,3 +1,37 @@
-"""NUMBER - уникальный идентификатор книги (её номер)
+import click
 
-Удаляет книгу из библиотеке по номеру. Если задан флаг -a , то удаляет все книги, очищая библиотеку."""
+import db
+
+
+@click.command()
+@click.option(
+    "--all",
+    "-a",
+    "delete_data",
+    is_flag=True,
+    default=False,
+    help="This flag for cleaning all database",
+)
+@click.option(
+    "--number",
+    "-n",
+    "book_id",
+    type=int,
+    help="Book ID for deleting the book",
+)
+def enter_point(book_id, delete_data):
+    if delete_data:
+        deleted_data = db.delete_all_data()
+        click.echo("Data base was cleaned")
+        click.echo(f"{deleted_data['book']} books deleted")
+        click.echo(f"{deleted_data['author']} authors deleted")
+    else:
+        is_correct = db.delete_book_by_id(book_id)
+        if is_correct:
+            click.echo("Book deleting was success")
+        else:
+            click.echo("Book was not found")
+
+
+if __name__ == "__main__":
+    enter_point()
