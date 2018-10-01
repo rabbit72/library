eb6a0933d923531c50136fb9a7b305fdab7b4813
Daniil Shadrin <rabbit72rus@gmail.com>
2018-10-01 15:00:05 +0300

    remove elasticsearch

--- dockerfile/migrations/01-сreate_author.sql
+++ dockerfile/migrations/01-сreate_author.sql
@@ -1,11 +1,16 @@
-CREATE TABLE "author" (
-	"id" serial NOT NULL,
-	"name" varchar(128) NOT NULL UNIQUE,
-  "last_update" timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	CONSTRAINT author_pk PRIMARY KEY ("id")
+CREATE TABLE author (
+	id serial NOT NULL,
+	name zdb.fulltext NOT NULL UNIQUE,
+  last_update timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	CONSTRAINT author_pk PRIMARY KEY (id)
 ) WITH (
   OIDS=FALSE
 );
+
+CREATE INDEX idx_author
+          ON author
+       USING zombodb ((author.*))
+        WITH (url='elasticsearch:9200/');
 
 CREATE TRIGGER last_update_modification_author
     BEFORE UPDATE ON author
--- dockerfile/migrations/02-create_book.sql
+++ dockerfile/migrations/02-create_book.sql
@@ -1,12 +1,17 @@
-CREATE TABLE "book" (
-	"id" serial NOT NULL,
-	"title" varchar(256) NOT NULL,
-	"year" int,
-  "last_update" timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	CONSTRAINT book_pk PRIMARY KEY ("id")
+CREATE TABLE book (
+	id serial NOT NULL,
+	title zdb.fulltext NOT NULL,
+	year int,
+  last_update timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	CONSTRAINT book_pk PRIMARY KEY (id)
 ) WITH (
   OIDS=FALSE
 );
+
+CREATE INDEX idx_book
+          ON book
+       USING zombodb ((book.*))
+        WITH (url='elasticsearch:9200/');
 
 CREATE TRIGGER last_update_modification_book
     BEFORE UPDATE ON book
